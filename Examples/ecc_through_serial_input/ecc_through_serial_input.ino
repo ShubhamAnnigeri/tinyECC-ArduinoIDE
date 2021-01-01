023f632de1f274acaed929ec6ec4e1cd779240d2
Sunit Raut <76087855+SunitRaut@users.noreply.github.com>
2021-01-01 17:14:33 +0530

    Update ecc_through_serial_input.ino

--- Examples/ecc_through_serial_input/ecc_through_serial_input.ino
+++ Examples/ecc_through_serial_input/ecc_through_serial_input.ino
@@ -1,3 +1,30 @@
+// **********************************************************************************
+// Author:    Shubham Annigeri	github.com/ShubhamAnnigeri
+// co-Author: Sunit Raut   	    github.com/SunitRaut
+// **********************************************************************************
+// License
+// **********************************************************************************
+// This program is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU General    
+// Public License as published by the Free Software       
+// Foundation; either version 3 of the License, or        
+// (at your option) any later version.                    
+//                                                        
+// This program is distributed in the hope that it will   
+// be useful, but WITHOUT ANY WARRANTY; without even the  
+// implied warranty of MERCHANTABILITY or FITNESS FOR A   
+// PARTICULAR PURPOSE. See the GNU General Public        
+// License for more details.                              
+//                                                        
+// Licence can be viewed at                               
+// http://www.gnu.org/licenses/gpl-3.0.txt
+//
+// Please maintain this license information along with authorship
+// and copyright notices in any redistribution of this code
+// **********************************************************************************
+// This file: Feed plaintext to microcontroller and see results of encryption, decryption, signature generation & verification
+// **********************************************************************************
+
 #include <tinyECC.h>
 
 tinyECC ecc;
